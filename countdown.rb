3242b396021daab1db7071bc6c272912e6bb37e0
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-10-28 00:59:42 -0400

    Update countdown.rb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
 def countdown
+    integer = 10
+    while integer <= 10
+        puts "#{integer} SECOND(S)!"
+        number -= 1
+        "HAPPY NEW YEAR!"
+    end
 end
+
+def countdown_with_sleep
+    integer = 5
+    while integer <= 5
+        puts "#{integer} SECOND(S)!"
+        number -= 1
+    end
+end    
